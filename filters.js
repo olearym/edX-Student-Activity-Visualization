1d9553512e2908723d8f0cabfd7b33edfa23ed14
Joe Hanley <jhanley@mit.edu>
2013-08-09 14:52:25 -0400

    Found filters.js

--- filters.js
+++ filters.js
@@ -1,13 +1,15 @@
 var filtered_data = data
 var currentView='all'
+
+//setupFilters builds the filter interface that appears below the graph.
 var setupFilters= function(){
     var timeFilterBar = $("<div id='time'>Filter by Time<select class='time'><option value='all'>All</option><option value='compWeek'>Compiled Weekly View</option><option value='2013-09-01,2013-09-07'>09/01 - 09/07</option><option value='2013-09-08,2013-09-14'>09/08 - 09/14</option><option value='2013-09-15,2013-09-21'>09/15 - 09/21</option><option value='2013-09-22,2013-09-28'>09/22 - 09/28</option></select></div>")
     var gradeFilter = $("<div id='grade-slider'><span class='grade-title'>Filter by Grade</span></div><div><input type='text' id='amount' style='border: 0; color: #000000;'></input></div>")
-//    var typeFilters = $('<div id="typeFilters"><form name="types" class="types"><input type="checkbox" class="video"  name ="video" checked=true value="video">Show Video Events<br><input type="checkbox" name="problem" class="problem" checked=true value="problem">Show Problem Events</form></div>')
+// var typeFilters = $('<div id="typeFilters"><form name="types" class="types"><input type="checkbox" class="video" name ="video" checked=true value="video">Show Video Events<br><input type="checkbox" name="problem" class="problem" checked=true value="problem">Show Problem Events</form></div>')
     var goButton = $("<button class='go btn' onclick='applyFilters()'>View Filtered Data</button>")
     $('.filter-div').append(timeFilterBar)
                     .append(gradeFilter)
-//                    .append(typeFilters)
+// .append(typeFilters)
                     .append(goButton)
     
     
@@ -19,7 +21,7 @@ var setupFilters= function(){
       max: 100,
       values: [0, 100 ],
       slide: function( event, ui ) {
-        $( "#amount" ).val(  + ui.values[ 0 ] + " - " + ui.values[ 1 ] );
+        $( "#amount" ).val( + ui.values[ 0 ] + " - " + ui.values[ 1 ] );
       }
     });
     $( "#amount" ).val( $( "#grade-slider" ).slider( "values", 0 ) +
@@ -29,21 +31,22 @@ var setupFilters= function(){
     
 }
 
+//applyFilters takes the data from the DOM elements set up in setupFilters, and returns a list with the appropriate filters applied. It uses the list "data" defined in seperate.js as the base list.
 var applyFilters=function(){
     var filtered_data=$.extend(true, [], data)
     var timeFilter = $('.time').val()
     var upper = $( "#grade-slider" ).slider( "values", 1 )
     var lower = $( "#grade-slider" ).slider( "values", 0 )
     var oldView= currentView
-//    var typesList = []
-//    if (types.video.checked==true){
-//        typesList.push('video')
-//    }
-//    if (types.problem.checked==true){
-//        typesList.push('problem')
-//    }
-//    
-//    filtered_data=sortByType(filtered_data,typesList)
+// var typesList = []
+// if (types.video.checked==true){
+// typesList.push('video')
+// }
+// if (types.problem.checked==true){
+// typesList.push('problem')
+// }
+//
+// filtered_data=sortByType(filtered_data,typesList)
     
     if (timeFilter != 'all'){
         currentView='compWeek'
