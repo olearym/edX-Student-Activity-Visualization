9459555228fa5695aadb6e2063e2c0a8a148d15b
Megan O'Leary <olearym@mit.edu>
2013-08-12 11:01:48 -0400

    added event handlers to dropdown and slider, chart is updated when one of the filters is changed

--- filters.js
+++ filters.js
@@ -5,12 +5,8 @@ var currentView='all'
 var setupFilters= function(){
     var timeFilterBar = $("<div id='time'>Filter by Time<select class='time'><option value='all'>All</option><option value='compWeek'>Compiled Weekly View</option><option value='2013-09-01T04:00:00,2013-09-08'>09/01 - 09/07</option><option value='2013-09-08T04:00:00,2013-09-15'>09/08 - 09/14</option><option value='2013-09-15T04:00:00,2013-09-22'>09/15 - 09/21</option><option value='2013-09-22T04:00:00,2013-09-29'>09/22 - 09/28</option></select></div>")
     var gradeFilter = $("<div id='grade-slider'><span class='grade-title'>Filter by Grade</span></div><div><input type='text' id='amount' style='border: 0; color: #000000;'></input></div>")
-// var typeFilters = $('<div id="typeFilters"><form name="types" class="types"><input type="checkbox" class="video" name ="video" checked=true value="video">Show Video Events<br><input type="checkbox" name="problem" class="problem" checked=true value="problem">Show Problem Events</form></div>')
-    var goButton = $("<button class='go btn' onclick='applyFilters()'>View Filtered Data</button>")
     $('.filter-div').append(timeFilterBar)
                     .append(gradeFilter)
-// .append(typeFilters)
-                    .append(goButton)
     
     
     
@@ -22,12 +18,16 @@ var setupFilters= function(){
       values: [0, 100 ],
       slide: function( event, ui ) {
         $( "#amount" ).val( + ui.values[ 0 ] + " - " + ui.values[ 1 ] );
+      },
+      stop: function(event, ui) {
+        applyFilters();
       }
     });
     $( "#amount" ).val( $( "#grade-slider" ).slider( "values", 0 ) +
       " - " + $( "#grade-slider" ).slider( "values", 1 ) );
   });
-    
+
+    $('.time').on("change", applyFilters)
     
 }
 
@@ -68,10 +68,18 @@ var applyFilters=function(){
     filtered_data=sortByGrade(filtered_data,lower,upper)
      
     if (currentView==oldView){
-        separate_charts.redraw(format_separated_data(filtered_data))
+        if (timeFilter == 'compWeek') {
+            separate_charts.redraw(data_process.averaged_data(format_separated_data(filtered_data), 5))
+        } else {
+            separate_charts.redraw(format_separated_data(filtered_data))
         }
+    }
     else{
-        separate_charts.setup(format_separated_data(filtered_data))
+        if (timeFilter == 'compWeek') {
+            separate_charts.setup(data_process.averaged_data(format_separated_data(filtered_data), 5))
+        } else {
+            separate_charts.setup(format_separated_data(filtered_data))
+        }
     }
                                     
 }
