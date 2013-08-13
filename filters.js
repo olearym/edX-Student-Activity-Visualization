69736661499caaf1b2134c75282678d4514539f5
Megan O'Leary <olearym@mit.edu>
2013-08-13 11:53:03 -0400

    changed filter names, moved order of filtering so joe's avg week still works

--- filters.js
+++ filters.js
@@ -3,8 +3,8 @@ var currentView='all'
 
 //setupFilters builds the filter interface that appears below the graph.
 var setupFilters= function(){
-    var timeFilterBar = $("<div id='time'>Filter by Time<select class='time'><option value='all'>All</option><option value='compWeek'>Average Week</option><option value='2013-09-01T04:00:00,2013-09-08'>Week 1: 09/01 - 09/07</option><option value='2013-09-08T04:00:00,2013-09-15'>Week 2: 09/08 - 09/14</option><option value='2013-09-15T04:00:00,2013-09-22'>Week 3: 09/15 - 09/21</option><option value='2013-09-22T04:00:00,2013-09-29'>Week 4: 09/22 - 09/28</option></select></div>")
-    var gradeFilter = $("<div id='grade-slider'><span class='grade-title'>Filter by Grade</span></div><div><input type='text' id='amount' style='border: 0; color: #000000;'></input></div>")
+    var timeFilterBar = $("<div id='time'>Time Range:<select class='time'><option value='all'>All</option><option value='compWeek'>Average Week</option><option value='2013-09-01T04:00:00,2013-09-08'>Week 1: 09/01 - 09/07</option><option value='2013-09-08T04:00:00,2013-09-15'>Week 2: 09/08 - 09/14</option><option value='2013-09-15T04:00:00,2013-09-22'>Week 3: 09/15 - 09/21</option><option value='2013-09-22T04:00:00,2013-09-29'>Week 4: 09/22 - 09/28</option></select></div>")
+    var gradeFilter = $("<div id='grade-slider'><span class='grade-title'>Grade Range:</span></div><div><input type='text' id='amount' style='border: 0; color: #000000;'></input></div>")
     $('.filter-div').append(timeFilterBar)
                     .append(gradeFilter)
     
@@ -33,11 +33,16 @@ var setupFilters= function(){
 
 //applyFilters takes the data from the DOM elements set up in setupFilters, and returns a list with the appropriate filters applied. It uses the list "data" defined in seperate.js as the base list.
 var applyFilters=function(){
+    by_grade.filterAll()
+    by_date.filterAll()
     var filtered_data=$.extend(true, [], data)
     var timeFilter = $('.time').val()
     var upper = $( "#grade-slider" ).slider( "values", 1 )
     var lower = $( "#grade-slider" ).slider( "values", 0 )
     var oldView= currentView
+
+    by_grade.filterRange([lower, upper])
+    filtered_data=by_grade.top(Infinity)
     
     if (timeFilter != 'all'){
         currentView='compWeek'
@@ -58,8 +63,6 @@ var applyFilters=function(){
         filtered_data = by_date.top(Infinity)
         
     }
-    by_grade.filterRange([lower, upper])
-    filtered_data=by_grade.top(Infinity)
      
     if (currentView==oldView){
         if (timeFilter == 'compWeek') {
